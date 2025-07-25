e62ee8211c99fa3406384259cd9694b538e964f3
beginnerones <huns0905@naver.com>
2025-07-25 14:50:19 +0900

    [Setting/#21] gpt prompt setting

--- src/main/java/capstone/_4/controller/impl/ScheduleController.java
+++ src/main/java/capstone/_4/controller/impl/ScheduleController.java
@@ -3,14 +3,12 @@ package capstone._4.controller.impl;
 import capstone._4.dto.ApiResponseDto;
 import capstone._4.dto.schedule.output.ScheduleResponseDto;
 import capstone._4.enums.ResponseEnum;
+import capstone._4.service.OpenAiService;
 import capstone._4.service.schedule.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -19,12 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
+    private final OpenAiService openAiService;
 
     @GetMapping("/all/search")
     public ResponseEntity<?> searchSchedule(@RequestParam Integer groupId){
         ScheduleResponseDto scheduleResponseDto= scheduleService.getSchedule(groupId);
         return ResponseEntity.ok().body(new ApiResponseDto<>(ResponseEnum.SUCCESS.getCode(),
                 ResponseEnum.SUCCESS.getMessage(),scheduleResponseDto ));
+    }
+
+    @PostMapping("/group/recom")
+    public ResponseEntity<?> recommendSchedule(){
+        String response= openAiService.createMessage();
+        return ResponseEntity.ok().body(new ApiResponseDto<>(ResponseEnum.SUCCESS.getCode(),
+                ResponseEnum.SUCCESS.getMessage(),response));
     }
 
 }
