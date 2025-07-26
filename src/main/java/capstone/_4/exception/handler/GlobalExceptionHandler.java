428cec9b7d41bc9b60fd521d51738274711f2c32
beginnerones <huns0905@naver.com>
2025-07-26 22:00:35 +0900

    [Fix/#25] 충돌 해결

--- src/main/java/capstone/_4/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/capstone/_4/exception/handler/GlobalExceptionHandler.java
@@ -19,7 +19,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> allException(DecryptionException e){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponseDto<>(ErrorCode.EXCEPTION.getStaus(),
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponseDto<>(ErrorCode.EXCEPTION.getStatus(),
                 ErrorCode.EXCEPTION.getMessage(),e.getMessage()));
     }
 
