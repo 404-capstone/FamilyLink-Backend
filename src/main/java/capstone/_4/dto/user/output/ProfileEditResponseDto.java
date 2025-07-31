dc5b81d412ce84511a10549148bcaa597e8916db
juhyeok <wngur3033@naver.com>
2025-07-31 17:27:14 +0900

    [Feat/#1] dto add

--- src/main/java/capstone/_4/dto/user/output/ProfileEditResponseDto.java
+++ src/main/java/capstone/_4/dto/user/output/ProfileEditResponseDto.java
@@ -11,4 +11,5 @@ public class ProfileEditResponseDto {
     private String username;  // 이름
     private Integer age;      // 나이대
     private String gender;    // 성별
+    private String image;     // 프로필
 }
