b9ff7ba715232b8e3ae803b16e39cb934fcba816
juhyeok <wngur3033@naver.com>
2025-08-08 17:26:25 +0900

    [Feat/#50]그룹 스케줄 생성 및 domain수정

--- src/main/java/capstone/_4/dto/schedule/output/GroupScheduleDto.java
+++ src/main/java/capstone/_4/dto/schedule/output/GroupScheduleDto.java
@@ -21,11 +21,11 @@ public class GroupScheduleDto {
     private String title;
 
     @Schema(description = "일정 시작일")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime startTime;
 
     @Schema(description = "일정 종료일")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime endTime;
 
     @Schema(description = "일정 내용")
