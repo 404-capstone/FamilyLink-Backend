a16ec2b14f1c0ee457281bd8fdc681c1408d4204
beginnerones <huns0905@naver.com>
2025-08-30 15:28:01 +0900

    [Feat/#110] dto수정

--- src/main/java/capstone/_4/dto/diary/output/EmotionResultDto.java
+++ src/main/java/capstone/_4/dto/diary/output/EmotionResultDto.java
@@ -10,6 +10,6 @@ import lombok.*;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EmotionResultDto {
-    private String label;
+    private String emotion;
     private Double percent;
 }
