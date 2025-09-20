917a2c50854dec0f9d6639d1a66105f73742bb7e
beginnerones <huns0905@naver.com>
2025-09-20 21:20:51 +0900

    [Fix/#127] group code search

--- src/main/java/capstone/_4/config/api/RedissonConfig.java
+++ src/main/java/capstone/_4/config/api/RedissonConfig.java
@@ -3,17 +3,20 @@ package capstone._4.config.api;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
 
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://127.0.0.1:6379")
+                .setAddress("redis://"+host+":6379")
         .setDatabase(0);
         return Redisson.create(config);
     }
