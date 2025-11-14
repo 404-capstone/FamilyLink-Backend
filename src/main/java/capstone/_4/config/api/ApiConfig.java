88aaa156c0c97c542bcaecf763e8da0c26e3ece7
beginnerones <huns0905@naver.com>
2025-11-14 19:44:17 +0900

    [Fix/#159] webclient 타임아웃 시간 늘리기 설정

--- src/main/java/capstone/_4/config/api/ApiConfig.java
+++ src/main/java/capstone/_4/config/api/ApiConfig.java
@@ -1,11 +1,19 @@
 package capstone._4.config.api;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ApiConfig {
@@ -21,8 +29,16 @@ public class ApiConfig {
     @Qualifier("OpenAiWebClient")
     @Bean
     public WebClient OpenAiWebClient() {
+
+        HttpClient httpClient= HttpClient.create().
+                option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofSeconds(90))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(90, TimeUnit.SECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(90, TimeUnit.SECONDS)));
         return WebClient.builder().baseUrl(gptUrl)
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + openAiKey)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 
